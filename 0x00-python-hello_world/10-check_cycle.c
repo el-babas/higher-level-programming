c18b2b99bc16df875c6f8f131657b740ff649c21
cristhian1107 <cristhian.cjaa@gmail.com>
2021-12-20 21:01:50 -0500

    10 Task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
+/**
+ * check_cycle -  singly linked list has a cycle in it
+ * @list: structut
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *l_flash, *l_zoom;
 
+	*l_flash = *l_zoom = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (l_flash && l_zoom && l_flash->next && l_zoom->next->next)
+	{
+		l_flash = l_flash->next;
+		l_zoom = l_zoom->next->next;
+		/* check if flash reached zoom */
+		if (l_flash == l_zoom)
+			return (1);
+	}
+	return (0);
 }

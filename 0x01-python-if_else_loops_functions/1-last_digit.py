f3b47d18f03cabe8c04077c647b55734e1563c41
cristhian1107 <cristhian.cjaa@gmail.com>
2021-12-21 13:21:44 -0500

    1 Task

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ldigit = number % 10
-print("Last digit of {:d} is".format(number), end=" ")
+"""Operador ternario"""
+ldigit = (number if number > 0 else -(number)) % 10
+ldigit = ldigit if number > 0 else -(ldigit)
+
+print("Last digit of {0:d} is {1:d}".format(number, ldigit), end=" ")
 if ldigit > 5:
     print("and is greater than 5")
 elif ldigit == 0:
     print("and is 0")
-elif ldigit < 6:
-    print("and is less than 6 and not 0")
 else:
-    print("")
+    print("and is less than 6 and not 0")

1265ad5e2891c80aa7a0d32df454023ceb96e678
cristhian1107 <cristhian.cjaa@gmail.com>
2021-12-23 12:14:05 -0500

    2 Task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,11 @@
 if __name__ == "__main__":
     import sys
     len_argv = len(sys.argv)
-    end_line = ":"
-    if (len_argv - 1) == 0:
-        end_line = "."
-    print("{0} arguments{1}".format(len_argv - 1, end_line))
+    argument = "arguments:"
+    if len_argv == 1:
+        argument = "arguments."
+    if len_argv == 2:
+        argument = "argument:"
+    print("{0} {1}".format(len_argv - 1, argument))
     for i in range(1, len_argv):
         print("{0}: {1}".format(i, sys.argv[i]))

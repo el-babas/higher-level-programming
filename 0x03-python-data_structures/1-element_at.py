d2c84e927ae984bb97702a98c823a6377ffb9e98
cristhian1107 <cristhian.cjaa@gmail.com>
2022-01-03 13:03:03 -0500

    1 Task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 def element_at(my_list, idx):
     if idx < 0:
-        return (None)
-
-    count = len(my_list)
-    if idx > count - 1:
-        return (None)
-
-    return (my_list[idx])
+        return None
+    if idx > len(my_list) - 1:
+        return None
+    return my_list[idx]

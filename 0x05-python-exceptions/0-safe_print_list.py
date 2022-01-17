6834ad06d68e796b6bb2ba0f0ad17a8e09597a7d
cristhian1107 <cristhian.cjaa@gmail.com>
2022-01-17 13:32:27 -0500

    0 - 1 Task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
             cont += 1
-        except:
+        except BaseException:
             continue
     print()
     return (cont)

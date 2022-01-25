35c0c6de5e7ebbf93f6a4b233d2a360f1f0781e2
cristhian1107 <cristhian.cjaa@gmail.com>
2022-01-25 17:25:02 -0500

    Advanced Tasks

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(str_array=[]):
+    str_array += ["Holberton"]
+    return ", ".join(str_array)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This file containts a class
+"""
+
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Method - Inicializate """
+        pass

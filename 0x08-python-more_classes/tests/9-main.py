1b09d6030c212ca444a2f7bf0e6a87dde2292380
cristhian1107 <cristhian.cjaa@gmail.com>
2022-01-24 22:14:33 -0500

    Sorting repository

--- 0x08-python-more_classes/tests/9-main.py
+++ 0x08-python-more_classes/tests/9-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 
-my_square = Rectangle.square(-5)
+my_square = Rectangle.square(5)
 print(
         "Area: {} - Perimeter: {}".format(
             my_square.area(),

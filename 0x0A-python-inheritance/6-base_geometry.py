535e133198e377570940045dd0eb3a12a9bc1894
cristhian1107 <cristhian.cjaa@gmail.com>
2022-01-31 16:45:39 -0500

    6 Task Add methos area in class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class:
+        a) BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """ Class - BaseGeometry
+    """
+
+    def area(self):
+        """ Method - In construction ...
+        """
+        raise Exception("area() is not implemented")

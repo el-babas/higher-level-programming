48d9645ca1aa0709215f56de391191586781b4f4
cristhian1107 <cristhian.cjaa@gmail.com>
2022-02-01 11:33:09 -0500

    [Add] New Files Add Files 	.gitignore 	README.md 	my_file_0.txt 	0-main.py 	0-read_file.py Add functions 	read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Functions:
+        a) read_file
+"""
+
+
+def read_file(filename=""):
+    """ Function: Read File and print it to stdout.
+
+    Args:
+        filename (str): The filename to read.
+
+    """
+    with open('my_file_0.txt', mode='r', encoding='utf-8') as f_txt:
+        print(f_txt.read(), end='')

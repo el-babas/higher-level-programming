6c7fb8f2a620a5ebb89181cd14c8c5d450925629
cristhian1107 <cristhian.cjaa@gmail.com>
2022-02-01 16:16:26 -0500

    [Add] 11 Task Changes to be committed: 	new file:   11-main.py 	new file:   11-student.py 	new file:   student.json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+""" Class:
+        a) Student
+"""
+
+
+class Student():
+    """ Class: Represents an object of type Student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Construct: Create object Student.
+
+        Args:
+            first_name (str): Name of the first student.
+            last_name (str): Name of the last student.
+            age (int): Years old to student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method: That retrieves a dictionary
+                    representation of a Student instance.
+
+        Returns: The dictionary description with simple data structure.
+                 If attrs is a list of strings,
+                 only attribute names contained in this list must be retrieved.
+        """
+        if (type(attrs) is list and all(type(key) == str for key in attrs)):
+            dict_obj = dict()
+            for key in attrs:
+                if hasattr(self, key):
+                    dict_obj[key] = getattr(self, key)
+            return (dict_obj)
+        return (self.__dict__.copy())
+
+    def reload_from_json(self, json):
+        """ Method: Replace all atributes with json values
+
+        Args:
+            json (str): JSON format object to student
+        """
+        for key in json:
+            setattr(self, key, json[key])

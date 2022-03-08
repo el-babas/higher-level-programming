13f137c4a149d217abd4b4160614b3e99e530b73
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-08 12:15:40 -0500

    [Add] 5 Task

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- ==== 5: FULL DESCRIPTION ====
+-- SQL Query that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Shows the CREATE TABLE statement that creates the named table.
+SHOW CREATE TABLE first_table;

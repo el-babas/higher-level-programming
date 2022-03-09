4b038309b35195544021b4c2721be2af0fc2a50c
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-09 18:59:46 -0500

    [Add] 0-5 Task  Changes to be committed: 	new file:   0-privileges.sql 	new file:   1-create_user.sql 	new file:   2-create_read_user.sql 	new file:   3-force_name.sql 	new file:   4-never_empty.sql 	new file:   5-unique_id.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- #### 3-ALWAYS A NAME #### --
+-- Script that creates the table force_name on your MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+    name VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- #### 4-ID CAN'T BE NULL #### --
+-- Script that creates the table id_not_null on your MySQL server.
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAULT 1,
+    name VARCHAR(256)
+);

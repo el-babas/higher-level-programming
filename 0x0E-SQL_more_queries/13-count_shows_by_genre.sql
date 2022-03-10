261318298ba1f71fbceb81300c7f90221e632c4a
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-09 19:50:25 -0500

    [Add] 10-12 Task

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- #### 13-NUMBER OF SHOWS BY GENRE #### --
+-- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT 
+	tv_genres.name AS genre,
+    COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;

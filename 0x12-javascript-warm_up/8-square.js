eede2a0238e9c2661c4c9435ac69706886cbe241
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-04 17:18:21 -0500

    Feat! 8 Task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,11 @@ const size = parseInt(process.argv[2]);
 if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0, s; i < size; i++) {
-    s = '';
+  for (let i = 0, simbol; i < size; i++) {
+    simbol = '';
     for (let j = 0; j < size; j++) {
-      s += 'X';
+      simbol += 'X';
     }
-    console.log(s);
+    console.log(simbol);
   }
 }

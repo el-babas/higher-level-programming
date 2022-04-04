5f5e429abb7202b9a40557c646d5f1bd04a9b856
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-04 17:00:26 -0500

    Feat! 5 Task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const my_number = parseInt(process.argv[2]);
-if (Number.isNaN(my_number)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + my_number);
-}
+const number = parseInt(process.argv[2]);
+console.log(isNaN(number) ? 'Not a number' : 'My number: ' + number);

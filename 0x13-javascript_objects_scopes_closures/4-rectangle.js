a5cc4e92ed82ec6912fe5165585839430eef62ef
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-05 12:30:14 -0500

    Feat! (Add) Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+/**
+ * Class Rectangle that defines a rectangle.
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const simbol = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(simbol.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;

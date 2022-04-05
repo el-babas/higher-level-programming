e55e15fbdb5fc908561f48640870a900d9b7c19c
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-05 12:09:15 -0500

    Feat! (Add) Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * Class Rectangle that defines a rectangle.
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

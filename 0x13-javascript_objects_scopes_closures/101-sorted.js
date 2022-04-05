728dff0fa8b24d4228d8919f4d29d3e0b94df307
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-05 18:44:07 -0500

    Feat! (Add) Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+/**
+ * Imports a dictionary of occurrences by user id and computes
+ *  a dictionary of user ids by occurrence.
+ */
+Object.keys(dict).map(function (key) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
+console.log(newDict);

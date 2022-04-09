3fe41672a141343be86fa4aae96e3d21986b1aad
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-09 16:48:26 -0500

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Functions:
+        a) mysqlconnect
+"""
+import sys
+import MySQLdb
+
+
+def mysqlconnect(username, password, database, port=3306):
+    """
+    Function:
+        Connect to MySQL.
+
+    Args:
+        username (str): username to connect to MySQL.
+        password (str): password to connect to MySQL.
+        database (str): database to connect to MySQL.
+        port (int): port to connect to MySQL.
+
+    Return:
+        Connect to MySQL.
+    """
+    db_connection = MySQLdb.connect(
+                        user=username,
+                        passwd=password,
+                        db=database,
+                        port=port)
+
+    return db_connection
+
+
+if __name__ == '__main__':
+    db_connection = mysqlconnect(sys.argv[1], sys.argv[2], sys.argv[3])
+    db_cursor = db_connection.cursor()
+    db_cursor.execute('''
+    SELECT * FROM states
+    WHERE name = %(filter1)s" ORDER BY id;
+    ''', {'filter1': sys.argv[4]})
+
+    states = db_cursor.fetchall()
+    for state in states:
+        print(state)
+
+    db_cursor.close()
+    db_connection.close()

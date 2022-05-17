641500d66693e9a19128bcbeeee23006ef26b8d0
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-17 13:50:39 -0500

    Feat (Add) Count films APIs nodejs 2

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const axios = require('axios').default;
 
-let count = 0;
+// let count = 0;
 // ID Wedge Antilles = 18.
 const idCharacter = 'https://swapi-api.hbtn.io/api/people/18/';
 // https://swapi-api.hbtn.io/api/films/
@@ -12,11 +12,14 @@ const api = process.argv[2];
 axios.get(api)
   .then(function (response) {
     // handle success
-    const films = response.data.results;
-    films.forEach(function (element) {
-      if (element.characters.includes(idCharacter)) count++;
-    });
-    console.log(count);
+    // const films = response.data.results;
+    // films.forEach(function (element) {
+    //   console.log(element.characters)
+    //   if (element.characters.includes(idCharacter)) count++;
+    // });
+    // console.log(count);
+    const films = response.data.results.filter(film => film.characters.includes(idCharacter));
+    console.log(films.length);
   })
   .catch(function (error) {
     // handle error

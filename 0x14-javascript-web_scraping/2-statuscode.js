d66b31d7a29ee2e0223478c95c08af69e95ba2d9
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-17 13:08:20 -0500

    Feat (Add) Get status code 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -9,9 +9,10 @@ axios.get(process.argv[2])
     // handle success
     console.log('code:', response.status);
   })
-  .catch(function () {
+  .catch(function (error) {
     // handle error
-    console.log('code: 404');
+    if (error.response) console.log('code:', error.response.status);
+    else console.log(error.message);
   })
   .then(function () {
     // always executed

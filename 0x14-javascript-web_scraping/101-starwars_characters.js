c5905935da7a1658c956082f569c3e9e2d8b6111
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-17 18:55:48 -0500

    Feat (Add) Request characters Async Await v2.0

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,18 +4,7 @@ const axios = require('axios').default;
 // Example: 3 = “Return of the Jedi”.
 const api = 'https://swapi-api.hbtn.io/api/films/';
 
-async function getFilms (url) {
-  try {
-    const response = await axios.get(url);
-    if (response.status === 200) {
-      return response.data;
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
-}
-
-async function getCharacter (url) {
+async function getData (url) {
   try {
     const response = await axios.get(url);
     if (response.status === 200) {
@@ -27,10 +16,10 @@ async function getCharacter (url) {
 }
 
 async function showCharacters (url) {
-  const film = await getFilms(url);
+  const film = await getData(url);
   const characters = film.characters;
   for (let i = 0; i < characters.length; i++) {
-    const character = await getCharacter(characters[i]);
+    const character = await getData(characters[i]);
     console.log(character.name);
   }
 }
